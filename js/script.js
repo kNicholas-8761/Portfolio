04c0883c7ede8ca9de5cd26319760697fe36820b
Kevin Brown <kamauri205@gmail.com>
2020-07-09 13:26:23 +1000

    Fixed navigation bugs

--- js/script.js
+++ js/script.js
@@ -99,8 +99,10 @@ $(document).ready(function () {
     var body = $("body");
 
     if ($(window).scrollTop() >= navTop) {
+      body.css("padding-top", nav.outerHeight() + "px");
       body.addClass("fixedNav");
     } else {
+      body.css("padding-top", 0);
       body.removeClass("fixedNav");
     }
   }
